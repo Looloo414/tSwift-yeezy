76f2a170850ef3f11054975dec7fe052da4ec079
Looloo414 <alexafaulkison@gmail.com>
2020-11-13 13:33:27 -0700

    Final

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const colorScheme = {
+    dark: "",
+    change: function() {
+        colorScheme.dark = colorScheme.dark ? "" : "dark";
+        body.setAttribute("class", colorScheme.dark);
+      },
+}
 
 /*-------------------------------- Variables --------------------------------*/
-
+const quotes = []
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -11,18 +16,79 @@ const swiftBtn = document.getElementById('swiftButton')
 const yeezyBtn = document.getElementById('yeezyButton')
 const container = document.getElementById('containerDiv')
 const lightDarkBtn = document.getElementById('lightDarkButton')
+const body = document.getElementById('body')
 
 /*----------------------------- Event Listeners -----------------------------*/
 swiftBtn.addEventListener('click', ()=> {
-    console.log('T-Swift Rocks')
+    fetch("https://api.taylor.rest/")
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let newQuote = {}
+        newQuote['artist'] = 'T-Swift'
+        newQuote['quote'] = data.quote
+        quotes.push(newQuote)
+        render()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
 yeezyBtn.addEventListener('click', ()=> {
-    console.log('Yeezy ROCKS!!!')
+    fetch("https://api.taylor.rest/")
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let newQuote = {}
+        newQuote['artist'] = 'Yeezy'
+        newQuote['quote'] = data.quote
+        quotes.push(newQuote)
+        render()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
 
-lightDarkBtn.addEventListener('click', ()=> {
-    console.log('Mode Swap GoGoGoGo!')
-})
+lightDarkBtn.addEventListener("click", colorScheme.change);
 
 /*-------------------------------- Functions --------------------------------*/
+function appendDiv(quote, artist, idx) {
+    let newDiv = document.createElement("div")
+    newDiv.innerHTML = `
+                        <div class="card h-100" id="${artist.toLowerCase()}">
+                            <div class="card-body">
+                                <blockquote class="blockquote mb-0">
+                                    <p>${quote}</p>
+                                    <footer class="blockquote-footer">${artist}</footer>
+                                </blockquote>
+                            </div>
+                            <button id="delButton" class="btn" onClick={deleteQuote(${idx})}>X</button>
+                        </div>    
+                        `
+    container.appendChild(newDiv)
+}
+
+function checkUserColorSchemePreference() {
+    if (
+      window.matchMedia("(prefers-color-scheme:dark)").matches &&
+      !colorScheme.dark
+    ) {
+      colorScheme.change();
+    }
+  }
+
+function deleteQuote(idx) {
+    quotes.splice(idx, 1)
+    render()
+  }
+
+function render() {
+    container.innerHTML = ""
+    quotes.forEach((quote, idx) => {
+      appendDiv(quote["quote"], quote["artist"], idx)
+    })
+}
